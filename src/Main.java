141a6369a64094f4e2f3c2e258cebe2934ec4e21
Natalia Alferova <natalia@alferova.me>
2021-10-27 11:36:30 +0200

    Total operation

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int coming = 500_000_000;
+        int total = balance + coming;
+        System.out.println(total);
+    }
 }
